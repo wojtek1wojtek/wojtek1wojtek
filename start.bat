9ddf42aa1886667e7fce8c17382c5cefcb533c8c
wojtek1wojtek <77987242+wojtek1wojtek@users.noreply.github.com>
2022-09-08 14:56:02 +0200

    Add files via upload

--- start.bat
+++ start.bat
@@ -1,26 +1,19 @@
-title Administrator Comand Tool
-echo of
-you are sure
-set 
-set /p wybieram:={y,n}:y start n close app
-echo
-przygotowywanie instalacji...
-ping localhost-n 2 >nul
-dir/s
-color 0A 
-pause
-dir/a
-pause
-echo instalacja twa
-echo loading...
-echo        [PORTAL1] - CONEDYT
-echo uruchamianie...
-echo comand
-pause
-echo @@@@@@@%*******@@@@@@@^@((((())))))(()()()()()()()()()(()()()()()()(()(()
-echo echo off
-echo conelost
-echo not fun microsoft net farmot
-echo istal to program lost
-pause
-        
+start regedit 
+start cmd
+start winver
+start cmd
+start cmd
+start cmd
+start cmd
+start cmd
+start cmd
+start winver
+start winver
+start winver
+start winver
+start winver
+start winver
+start winver
+start winver
+start winver
+start winver
